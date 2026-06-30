4d896edccb5841abdbcf11b7c317b7c18f8f303a
fotofabritz-ux <fotofabritz-ux@users.noreply.github.com>
2026-06-30 03:42:35 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict quFlKlkE72VtSBhrdVHy8seFWEusfT1hR7SZF4KAa1RCLuwEOo47nmk1bO6Lxbb
+-- \restrict b1kcwUTxcaH9ItbUPxCMVuMLE6A0D9lPCuotm8zwECvhfjjlShPsJ8DwU8gAxF4
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -33,7 +33,7 @@ COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "
 -- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at", "custom_claims_allowlist") FROM stdin;
 \.
 
 
@@ -3314,6 +3314,6 @@ SELECT pg_catalog.setval('"public"."platzdaten_NR_seq"', 1780872102056, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict quFlKlkE72VtSBhrdVHy8seFWEusfT1hR7SZF4KAa1RCLuwEOo47nmk1bO6Lxbb
+-- \unrestrict b1kcwUTxcaH9ItbUPxCMVuMLE6A0D9lPCuotm8zwECvhfjjlShPsJ8DwU8gAxF4
 
 RESET ALL;
